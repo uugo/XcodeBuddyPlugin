2dcd4e5a1832f3e79401fe4c7b93656446dadee6
uugooo <uugooo@163.com>
2016-04-07 16:52:49 +0800

    add SendProjectFilesWindownController

---
+++ XcodeBuddyPlugin/WindowController/SendProjectFilesWindowController.h
@@ -0,0 +1,19 @@
+//
+//  SendProjectFilesWindowController.h
+//  XcodeBuddyPlugin
+//
+//  Created by lizhen on 16/4/1.
+//  Copyright © 2016年 crafthm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT NSString* StartString;
+FOUNDATION_EXPORT NSString* EndString;
+FOUNDATION_EXPORT NSString* TotalString;
+
+@interface SendProjectFilesWindowController : NSWindowController
+@property (strong) IBOutlet NSTextView *sendedFilelistTextView;
+@property (strong) IBOutlet NSButton *closeButton;
+
+@end
---
+++ XcodeBuddyPlugin/WindowController/SendProjectFilesWindowController.m
@@ -0,0 +1,28 @@
+//
+//  SendProjectFilesWindowController.m
+//  XcodeBuddyPlugin
+//
+//  Created by lizhen on 16/4/1.
+//  Copyright © 2016年 crafthm. All rights reserved.
+//
+
+#import "SendProjectFilesWindowController.h"
+
+NSString const *StartString=@"Begin to send files.";
+NSString const *EndString=@"End.";
+NSString const *TotalString=@"Total:";
+
+@interface SendProjectFilesWindowController()
+
+@end
+
+@implementation SendProjectFilesWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    
+}
+
+@end
